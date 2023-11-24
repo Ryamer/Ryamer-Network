262bd972f88b5a231a8a1fe99e98a6c9ef2963eb
Rishi Panthee <rrpanthe@ncsu.edu>
2023-11-23 22:19:48 -0600

    Save a months worth and collect data every 10 minutes

--- app.js
+++ app.js
@@ -6,22 +6,22 @@ const fs = require("fs");
 
 let history = [];
 
-async function getAndSaveData(){
+async function getAndSaveData() {
   axios("https://akvorado.ryamer.com/api/v0/console/widget/top/etype?0").then((res) => {
     const data = res.data;
     let IPv6Percent = 0;
     let IPv4Percent = 0;
     const top = data.top;
-    for (const eType of top){
-      if (eType.name === "IPv4"){
+    for (const eType of top) {
+      if (eType.name === "IPv4") {
         IPv4Percent = parseFloat(eType.percent.toFixed(3));
       }
-      if (eType.name === "IPv6"){
+      if (eType.name === "IPv6") {
         IPv6Percent = parseFloat(eType.percent.toFixed(3));
       }
     }
     const now = Math.floor(Date.now());
-    history.push({time : now, ipv4 : IPv4Percent, ipv6 : IPv6Percent });
+    history.push({ time: now, ipv4: IPv4Percent, ipv6: IPv6Percent });
     writeHistory();
   });
 }
@@ -32,17 +32,18 @@ function readHistory() {
   return history;
 }
 
-function writeHistory(){
+function writeHistory() {
+  history = history.slice(-4032)
   fs.writeFileSync("./history.json", JSON.stringify(history));
   return true;
 }
 
-async function startUp(){
+async function startUp() {
   await readHistory();
   getAndSaveData();
   setInterval(() => {
     getAndSaveData();
-  }, 1000 * 60);
+  }, 1000 * 60 * 10);
 }
 
 startUp();
@@ -52,14 +53,14 @@ startUp();
 // START WEB SERVER
 
 const app = express();
-  
+
 app.get("/*", (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.json(history);
 });
 
 app.listen(5002, () => {
-  
+
 });
 
 // END WEB SERVER
