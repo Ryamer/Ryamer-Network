383fb6c491aff094857300661946d5b13e81dfbf
Rishi Panthee <github@rishipanthee.com>
2024-03-07 17:58:35 -0600

    yoink out apexcharts

--- UI/src/pages/stats.tsx
+++ UI/src/pages/stats.tsx
@@ -1,6 +1,5 @@
 import Layout from '@theme/Layout';
 import axios from 'axios';
-import Chart from "react-apexcharts";
 import { useEffect, useState } from 'react';
 
 export default function stats(): JSX.Element {
@@ -22,66 +21,10 @@ export default function stats(): JSX.Element {
           const firstDay = final[0][0];
           const lastDay = final[final.length - 1][0];
           average = average / data.length;
-          setSeries([{
-            name: "IPv6 Usage %",
-            data: final
-          }]);
-          setOptions({
-            chart: {
-              id: 'area-datetime',
-              height: 350,
-              zoom: {
-                enabled: true,
-                autoScaleYaxis: true
-              }
-            },
-            annotations: {
-              yaxis: [{
-                y: average,
-                borderColor: '#999',
-                label: {
-                  show: true,
-                  text: 'Average',
-                  style: {
-                    color: "#fff",
-                    background: '#00E396'
-                  }
-                }
-              }],
-              xaxis: []
-            },
-            dataLabels: {
-              enabled: false
-            },
-            markers: {
-              size: 0,
-              style: 'hollow',
-            },
-            xaxis: {
-              type: 'datetime',
-              min: new Date().getTime(),
-              tickAmount: 6,
-            },
-            tooltip: {
-              x: {
-                format: 'dd MMM yyyy'
-              }
-            },
-            fill: {
-              type: 'gradient',
-              gradient: {
-                shadeIntensity: 1,
-                opacityFrom: 0.7,
-                opacityTo: 0.9,
-                stops: [0, 100]
-              }
-            },
-            title: {
-              text: "IPv6 Usage",
-              align: 'center',
-            }
-          });
-        })
+          const xAxis = final.map((item) => new Date(item[0]))
+          const yAxis = final.map((item) => item[1])
+
+        });
       }
 
   return (
@@ -89,16 +32,7 @@ export default function stats(): JSX.Element {
       title={`Ryamer Stats`}
       description="See some of our public network stats">
       <main>
-      <div className="row">
-          <div className="mixed-chart">
-            <Chart
-              options={options}
-              series={series}
-              type="area"
-              width="1000"
-            />
-          </div>
-        </div>
+
       </main>
     </Layout>
   );
