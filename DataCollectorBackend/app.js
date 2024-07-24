2c725b32dfac1443adac9754b751e5544b6dd885
Rishi556 <github@rishipanthee.com>
2024-07-24 18:07:56 -0500

    Replace Axios On Data Collector Backend (#17)

--- DataCollectorBackend/app.js
+++ DataCollectorBackend/app.js
@@ -1,4 +1,3 @@
-const axios = require("axios");
 const config = require("./config.json");
 const express = require('express');
 const fs = require("fs");
@@ -8,23 +7,22 @@ const fs = require("fs");
 let history = [];
 
 async function getAndSaveData() {
-  axios(config.statsCollector.akvoradoAPIBaseURL + "/v0/console/widget/top/etype?0").then((res) => {
-    const data = res.data;
-    let IPv6Percent = 0;
-    let IPv4Percent = 0;
-    const top = data.top;
-    for (const eType of top) {
-      if (eType.name === "IPv4") {
-        IPv4Percent = parseFloat(eType.percent.toFixed(3));
-      }
-      if (eType.name === "IPv6") {
-        IPv6Percent = parseFloat(eType.percent.toFixed(3));
-      }
+  const apiResponse = await fetch(config.statsCollector.akvoradoAPIBaseURL + "/v0/console/widget/top/etype?0");
+  const data = await apiResponse.json();
+  let IPv6Percent = 0;
+  let IPv4Percent = 0;
+  const top = data.top;
+  for (const eType of top) {
+    if (eType.name === "IPv4") {
+      IPv4Percent = parseFloat(eType.percent.toFixed(3));
     }
-    const now = Math.floor(Date.now());
-    history.push({ time: now, ipv4: IPv4Percent, ipv6: IPv6Percent });
-    writeHistory();
-  });
+    if (eType.name === "IPv6") {
+      IPv6Percent = parseFloat(eType.percent.toFixed(3));
+    }
+  }
+  const now = Math.floor(Date.now());
+  history.push({ time: now, ipv4: IPv4Percent, ipv6: IPv6Percent });
+  writeHistory();
 }
 
 function readHistory() {
